1e5c07318875cb4acec75f46be4f551b485359cd
Oleksandr_Melashchenko <Oleksandr_Melashchenko@epam.com>
2018-09-05 16:53:42 +0300

    Initial commit

---
+++ src/main/kotlin/com/melalex/debts/DebtsApplication.kt
@@ -0,0 +1,11 @@
+package com.melalex.debts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DebtsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DebtsApplication>(*args)
+}
---
+++ src/test/kotlin/com/melalex/debts/DebtsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.melalex.debts
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DebtsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
