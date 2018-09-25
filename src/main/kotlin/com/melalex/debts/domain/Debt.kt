dc61d042d02df3a3b476fdf8aada3032377690b0
Oleksandr_Melashchenko <Oleksandr_Melashchenko@epam.com>
2018-09-25 12:21:43 +0300

    Add find debts query

--- src/main/kotlin/com/melalex/debts/domain/Debt.kt
+++ src/main/kotlin/com/melalex/debts/domain/Debt.kt
@@ -3,8 +3,6 @@ package com.melalex.debts.domain
 import org.neo4j.ogm.annotation.EndNode
 import org.neo4j.ogm.annotation.RelationshipEntity
 import org.neo4j.ogm.annotation.StartNode
-import java.math.BigDecimal
-import java.util.*
 import javax.money.MonetaryAmount
 
 @RelationshipEntity(type = LENTS)
--- src/main/kotlin/com/melalex/debts/repositories/UserRepository.kt
+++ src/main/kotlin/com/melalex/debts/repositories/UserRepository.kt
@@ -6,8 +6,17 @@ import org.springframework.data.neo4j.annotation.Query
 import org.springframework.data.neo4j.repository.Neo4jRepository
 import java.util.*
 
+// @formatter:off
+
+const val FIND_DEBTS_QUERY: String =
+                "MATCH (borrower:User)-[rel:LENTS]->(debtor:User) " +
+                "WHERE borrower.id = {0} AND debtor.id = {1} " +
+                "RETURN rel"
+
+// @formatter:on
+
 interface UserRepository : Neo4jRepository<User, Long> {
 
-    @Query("")
-    fun findDebts(borrower: User, debtor: User) : Optional<Debt>
+    @Query(FIND_DEBTS_QUERY)
+    fun findDebts(borrower: User, debtor: User): Optional<Debt>
 }
